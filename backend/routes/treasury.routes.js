715805a8cd236bc20b2f18ba5fc061d1b93de8f7
dedoo90 <dedoo90@gmail.com>
2026-07-30 22:49:40 +0300

    feat(backend): implement Treasury controller

---
+++ backend/routes/treasury.routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const ctrl = require('../controllers/treasury.controller');
+
+router.get('/', ctrl.list);
+router.get('/stats', ctrl.getStats);
+router.get('/:id', ctrl.getById);
+router.post('/', ctrl.create);
+router.put('/:id', ctrl.update);
+router.delete('/:id', ctrl.remove);
+
+module.exports = router;
